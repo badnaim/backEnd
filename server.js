10fef36b538e53f2fc0fa75d98699cd122221b20
badnaim <batnyaam.tumurbat@gmail.com>
2023-01-25 12:41:57 +0800

    b

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/products", (request, response) => {
 // app.get("/products", (request, response) => {
 //   console.log("GET products huselt orj irlee 1");
 //   response.status(200).send("products");
-// dd
+// d
 // });
 
 //api - access point interface - newtreh tseg
